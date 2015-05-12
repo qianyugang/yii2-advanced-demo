187fd4dbd6863a4f826f581dcfd05096ebbbe641
forecho <caizhenghai@gmail.com>
2015-05-12 10:34:08 +0800

    更新命令

--- update.sh
+++ update.sh
@@ -3,7 +3,12 @@
 # @Date:   2015-05-11 21:22:05
 # @Last Modified by:   forecho
 # @Last Modified time: 2015-05-11 21:27:37
+unzip yii2-advanced-demo.zip
+cd _yii2
+composer update
+cd ..
 zip -r yii2-advanced-demo.zip _yii2
 git add --all
-git commit -m "update@$(date +%Y-%m-%d_%H%M%S)"
+git commit -m "更新$(date +%Y-%m-%d_%H%M%S)"
 git push -f
+rm -rf _yii2
